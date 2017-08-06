ff259b148cbc83b396d4c5a29c9738d9388b06f2
Jonathan Whiting <jonathan@ignika.com>
2017-08-06 14:02:19 +0100

    cut out superfluous input stuff, updated libpng url

--- get_inputs_osx.sh
+++ get_inputs_osx.sh
@@ -2,15 +2,9 @@
 set -e
 mkdir input
 pushd input
-git clone git://github.com/martine/ninja.git
-pushd ninja
-git checkout release
-./configure.py --bootstrap
-popd
-curl -O https://raw.githubusercontent.com/martine/ninja/master/misc/ninja_syntax.py
-curl -O ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng12/libpng-1.2.53.tar.gz
-tar -xzf libpng-1.2.53.tar.gz
-ln -s libpng-1.2.53 libpng
+curl -O ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng12/libpng-1.2.57.tar.gz
+tar -xzf libpng-1.2.57.tar.gz
+ln -s libpng-1.2.57 libpng
 pushd libpng
 make -f scripts/makefile.darwin
 popd
